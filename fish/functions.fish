9bfa8b3c2ff5a9adf2c53fd45dcf98ecae067240
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-07-28 22:40:29 +0900

    fish prompt

--- fish/functions.fish
+++ fish/functions.fish
@@ -1,4 +1,4 @@
 function fish_prompt --description 'define the appearance of the command line prompt by @hiroga_cc'
-    printf '%s%s%s' (set_color white -b 666666)" "(date '+%Y-%m-%d %H:%M:%S')" " (set_color black -b E5E5E5)" "(prompt_pwd)" " (set_color black -b 8CF77C)" "(__fish_git_prompt | sed -e 's/ (//' -e 's/)$//')" " (set_color normal)(echo " \$ ")
+    printf '%s%s%s' (set_color white -b 666666)" "(date '+%Y-%m-%d %H:%M:%S')" " (set_color black -b E5E5E5)" "(basename $PWD)" " (set_color black -b yellow)" "(__fish_git_prompt | sed -e 's/ (//' -e 's/)$//')" " (set_color normal)(echo " \$ ")
 end
 funcsave fish_prompt
