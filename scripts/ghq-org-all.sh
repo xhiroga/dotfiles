60a2d950923a76735a1cae0d48726fa6b10f49e9
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-10-23 18:58:45 +0900

    remove scripts

--- scripts/ghq-org-all.sh
+++
@@ -1,6 +0,0 @@
-
-PERSONAL_ACCESS_TOKEN=$1
-ORG=$2
-curl -s "https://api.github.com/orgs/${ORG}/repos?access_token=${PERSONAL_ACCESS_TOKEN}&type=all" \
-    | jq .[].full_name -r \
-    | ghq import
