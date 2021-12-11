7818d0d1faac6c2b4ecedf8cb21c2aa880b61e42
xhiroga <13391129+xhiroga@users.noreply.github.com>
2021-12-12 08:53:00 +0900

    feat: ghq keybinds

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,6 +1,28 @@
 bindkey -e
+
 bindkey "^R" history-incremental-search-backward
-bindkey "^P" history-search-backward
+
+function ghq_peco() {
+    REPO=$(ghq list -p | peco)
+    if [ "$REPO" ]; then
+        cd "$REPO" || return
+    fi
+    zle accept-line
+}
+zle -N ghq_peco
+bindkey "^g^g" ghq_peco
+
+# Inspired by [GitHub謹製のghコマンドとpecoを組み合わせて、高速にPull Requestのブランチにチェックアウトする。 \- 文字っぽいの。](https://fromatom.hatenablog.com/entry/2020/03/31/135410)
+function peco-checkout-pull-request () {
+    local selected_pr_id=$(gh pr list | peco | awk '{ print $1 }')
+    if [ -n "$selected_pr_id" ]; then
+        BUFFER="gh pr checkout ${selected_pr_id}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-checkout-pull-request
+bindkey "^g^p" peco-checkout-pull-request
 
 ## Regerences
 #
