f4633168176c63ef817a5026c76d63f794dd324b
xhiroga <13391129+xhiroga@users.noreply.github.com>
2021-12-12 08:45:52 +0900

    feat: set emacs keybinding as default

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,2 +1,7 @@
+bindkey -e
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
+
+## Regerences
+#
+# - [zsh: 18 Zsh Line Editor](https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html)
